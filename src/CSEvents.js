d85f70ca9c83073c8b4815a98bd6abdd35702613
arbitroy <austinndauwa@gmail.com>
2023-11-03 09:23:28 +0300

    Updated the events page

--- src/CSEvents.js
+++ src/CSEvents.js
@@ -1,5 +1,4 @@
-'use clients'
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import ImageSlider from './ImageSlider';
 import image1 from './assets/7/adedotun-adegborioye-pVSjlshmGs8-unsplash.jpg';
 import image2 from './assets/7/alekon-pictures-gqX0rPCmdiU-unsplash.jpg';
@@ -9,153 +8,88 @@ import image5 from './assets/7/richie-lugo-uSC6z5ajEVM-unsplash.jpg';
 import image6 from './assets/7/vidar-nordli-mathisen-QsDArmUhlAE-unsplash.jpg';
 import image7 from './assets/7/black-boy-posing-with-headphones.jpg';
 
+const cardList = [
+  {
+    img: image1,
+    name: "Growth",
+    description: "Our group of specialists will collaborate with you to develop a personalized strategy aimed at guiding you toward success through incremental progress.",
+    venue: "Ridgewood Hotel",
+    price: {
+      single: 350,
+      couple: 650,
+      gate: 500,
+    },
+    contact: "0775902389",
+    time: "7pm till late",
+  },
+  {
+    img: image2,
+    name: "Auditions",
+    description: "Display your talent",
+    venue: "Ridgewood Hotel",
+    price: {
+      single: 350,
+      couple: 650,
+      gate: 500,
+    },
+    contact: "0775902389",
+    time: "7pm till late",
+  },
+];
+
 function CSEvents() {
-  const [category, setCategory] = useState('');
-  const [site, setSite] = useState('');
-  const [search, setSearch] = useState('');
-  const [events, setEvents] = useState([]);
-  const [categories, setCategories] = useState([]); // Define categories state
-  const [sites, setSites] = useState([]); // Define sites state
-
-  // Create an array for the new events
-  const newEvents = [
-    {
-      name: 'Myusiki Presents: The Asante Sol Fest',
-      date: '3rd November 2023',
-      time: '7pm to Late',
-      location: 'Ridgewood Hotel, King’ong’o Nyeri',
-      link: 'URL_for_tickets', // Add the URL for tickets here
-      image: image6, // Add the image URL for this event
-    },
-    {
-      name: 'Seven Days of Sol: Day 1 - Sauti Sol Covers #singlikesol',
-      date: 'Saturday 28th October 2023',
-      time: '',
-      location: '',
-      link: 'URL_for_event_1', // Add the URL for this event
-      image: image2, // Add the image URL for this event
-    },
-    // Add the rest of the new events here
-  ];
-
-  useEffect(() => {
-    // Fetch data for events, categories, and sites here
-    // Replace the following placeholders with your actual data fetching logic
-    const dummyData = {
-      events: [], // Array of event objects
-      categories: [], // Array of category names
-      sites: [], // Array of site names
-    };
-
-    // Combine the existing events with the new events
-    const allEvents = [...dummyData.events, ...newEvents];
-
-    setEvents(allEvents);
-    setCategories(dummyData.categories);
-    setSites(dummyData.sites);
-  }, [newEvents]);
+  const [hoveredCard, setHoveredCard] = useState(null);
+  const images = [image1, image2, image3, image4, image5, image6, image7];
 
   return (
-    <div className="container mx-auto rounded pt-36 sm:pl-20 sm:pt-36 sm:pr-4 max-w-xl lg:max-w-4xl">
-      <ImageSlider
-        images={[image1, image2, image3, image4, image5, image6, image7]}
-      />
-      <div className="mt-16 mb-8 flex flex-col sm:grid sm:grid-cols-2">
-        <div>
-          <select
-            value={category}
-            onChange={(e) => setCategory(e.target.value)}
-            className="p-4 mr-3 mb-3 bg-red-400 rounded-lg text-white font-bold"
+    <div className=''>
+      <ImageSlider images={images} />
+      <div className="flex flex-wrap my-8">
+        {cardList.map((card, index) => (
+          <div
+            key={index}
+            className={`w-full md:w-1/2 lg:w-1/3 px-4 mb-8`}
           >
-            <option value="">Filter by Category</option>
-            {categories.map((category, index) => (
-              <option key={index} value={category}>
-                {category}
-              </option>
-            ))}
-          </select>
-          {/* Sites filter */}
-          <select
-            value={site}
-            onChange={(e) => setSite(e.target.value)}
-            className="p-4 mb-3 bg-red-400 rounded-lg text-white font-bold"
-          >
-            <option value="">Filter by Site</option>
-            {sites.map((site, index) => (
-              <option key={index} value={site}>
-                {site}
-              </option>
-            ))}
-          </select>
-        </div>
-        {/* Search bar */}
-        <div>
-          <input
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-            placeholder="Search events..."
-            type="search"
-            className="p-4 mt-2 sm:mt-0 w-full bg-gray-200 rounded-lg"
-          />
-        </div>
-      </div>
-      <div className="sm:flex sm:flex-wrap w-full ">
-        {events.length === 0 ? ( // Check if there are no events
-          <p className="text-center text-2xl text-white p-36">
-            Sorry, no events yet
-          </p>
-        ) : (
-          events.map((event, index) => (
-            <a
-              key={index}
-              href={event.link}
-              target="_blank"
-              className="mx-auto"
-              rel="noreferrer"
+            <div
+              className={`relative overflow-hidden rounded-md border bg-white border-gray-300 transition duration-300 ${
+                hoveredCard === index ? 'shadow-lg' : ''
+              }`}
+              onMouseEnter={() => setHoveredCard(index)}
+              onMouseLeave={() => setHoveredCard(null)}
             >
-              <div className="flex sm:flex-col sm:h-80 rounded-2xl bg-red-500 mb-6 sm:transform transition duration-500 opacity-90 hover:opacity-100 hover:scale-105">
-                {/* Event image as top half of card */}
-                <div className="h-16 w-24 sm:h-40 sm:w-60 flex flex-shrink-0">
-                  <img
-                    className="mx-auto rounded-l-2xl sm:rounded-bl-none sm:rounded-tr-2xl object-cover"
-                    src={event.image}
-                    alt={event.name}
-                  />
+              <img
+                src={card.img}
+                alt={card.name}
+                className="w-full h-48 object-cover"
+              />
+              <div className={`p-4 ${hoveredCard === index ? 'h-auto' : 'h-24'}`}>
+                <div
+                  className={`hover:bg-gray-100 ${
+                    hoveredCard === index ? 'bg-gray-100' : ''
+                  }`}
+                >
+                  <h3 className="text-xl text-red-500 font-semibold">{card.name}</h3>
+                  <p className={`mt-2 text-gray-700`}>
+                    {card.description}
+                  </p>
                 </div>
-                {/* Bottom half of card */}
-                <div className="flex flex-col justify-center sm:text-center sm:mt-3 ml-6 sm:ml-0 text-white">
-                  {/* Event name */}
-                  <div className="overflow-hidden h-6 sm:overflow-visible sm:h-full sm:w-52 sm:mx-auto">
-                    <h3 className="text-base lg:text-lg font-bold text-red-300">
-                      {event.name}
-                    </h3>
-                  </div>
-                  {/* Text information section */}
+                {hoveredCard === index && (
                   <div>
-                    <div className="mt-2 max-h-5 sm:max-h-full overflow-hidden w-full">
-                      <p className="text-sm inline sm:block">
-                        <i className="fa fa-calendar-day"></i>&ensp;
-                        <span
-                          dangerouslySetInnerHTML={{ __html: event.date }}
-                        ></span>
-                        <span className="sm:hidden">&emsp;</span>
-                      </p>
-                      <p className="text-sm inline sm:inline-block">
-                        <i className="far fa-clock"></i>&ensp;
-                        <span>{event.time}</span>
-                        <span className="sm:hidden">&emsp;</span>
-                      </p>
-                      <p className="text-sm inline sm:block mt-1 italic">
-                        <i className="fas fa-map-marker-alt"></i>&ensp;
-                        <span>{event.location}</span>
-                      </p>
-                    </div>
+                    <p>Venue: {card.venue}</p>
+                    <p>Price:</p>
+                    <ul>
+                      <li>Single: {card.price.single}</li>
+                      <li>Couple: {card.price.couple}</li>
+                      <li>Gate: {card.price.gate}</li>
+                    </ul>
+                    <p>Contact: {card.contact}</p>
+                    <p>Time: {card.time}</p>
                   </div>
-                </div>
+                )}
               </div>
-            </a>
-          ))
-        )}
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -4,103 +4,139 @@ import React, { useState, useEffect } from 'react';
 export default function ImageSlider({ images }) {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 6000);
+  // Function to get content for the current index
+  function getContentForIndex(index) {
+    const contentMap = {
+      0: (
+        <div className="text-center">
+          <p className="font-bold text-3xl">
+            Day 1 - #singlikesol (challenge, shorts, etc.)
+          </p>
+          <p className="pt-4 text-lg">
+            This day is characterized by covers of any of the Sauti Sol hits. At
+            least five from Myusiki with an interesting twist to them. At least
+            three group and two solos but as many as possible.
+          </p>
+        </div>
+      ),
+      1: (
+        <div className="text-center">
+          <p className="font-bold text-4xl">Day 2 - #rewritesol</p>
+          <p className="pt-4 text-xl">
+            This day will be characterized by remixed versions of Sauti Sol songs.
+            At least five from Myusiki with an interesting twist to them. At least
+            three group and two solos but as many as possible.
+          </p>
+        </div>
+      ),
+      2: (
+        <div className="text-center">
+          <p className="font-bold text-4xl">Day 3 - #shikanishasol</p>
+          <p className="pt-4 text-xl">
+            This day will involve the writing of mashups using the various Sauti
+            Sol songs. At least five from Myusiki with an interesting twist to
+            them. At least three group and two solos but as many as possible.
+          </p>
+        </div>
+      ),
+      3: (
+        <div className="text-center">
+          <p className="font-bold text-4xl">Day 4 - #tuimbesol</p>
+          <p className="pt-4 text-xl">
+            This will be characterized by acapella song versions of Sauti Sol. At
+            least five from Myusiki with an interesting twist to them. At least
+            three group and two solos but as many as possible.
+          </p>
+        </div>
+      ),
+      4: (
+        <div className="text-center">
+          <p className="font-bold text-4xl">Day 5 - #bonganasol</p>
+          <p className="pt-4 text-xl">
+            This day will be characterized by people giving their testimonies of
+            what Sauti Sol has done for them and the impact Sauti Sol has had in
+            their lives.
+          </p>
+        </div>
+      ),
+      5: (
+        <div className="text-center">
+          <p className="font-bold text-4xl">Day 6 - #myfavouritesol</p>
+          <p className="pt-4 text-xl">
+            This will be characterized by people singing their favorite Sauti Sol
+            fam and saying why they like it and why it is their favorite.
+          </p>
+        </div>
+      ),
+      6: (
+        <div className="text-center">
+          <p className="font-bold text-4xl">Day 7 - #asantesolfest</p>
+          <p className="pt-4 text-xl">
+            This day is to just thank Sauti Sol for the great work they have done
+            and sing their songs together. It is also where we will launch the
+            Asante Sol album.
+          </p>
+        </div>
+      ),
+    };
 
-    return () => clearInterval(interval);
-  }, [images]);
+    return contentMap[index] || <div></div>;
+  }
+
+  // Function to navigate to the next image
+  function nextImage() {
+    if (currentImageIndex < images.length - 1) {
+      setCurrentImageIndex(currentImageIndex + 1);
+    }
+  }
+
+  // Function to navigate to the previous image
+  function prevImage() {
+    if (currentImageIndex > 0) {
+      setCurrentImageIndex(currentImageIndex - 1);
+    }
+  }
 
   return (
-    <div className="image-slider relative">
-  <div className="relative">
-    <img src={images[currentImageIndex]} alt="" className="w-full" />
-    <div className="absolute top-0 left-0 w-full h-full bg-opacity-70 bg-black text-white p-6 flex justify-center items-center">
-      <p className="text-lg font-bold text-center">
-        {getContentForIndex(currentImageIndex)}
-      </p>
+    <div className="pt-8 md:pt-48 pb-60 w-full h-full flex items-center justify-center bg-gradient-to-br from-purple-500 to-pink-500 font-montserrat relative">
+      <div className="w-96 h-96 relative perspective-3d transform-style-preserve-3d">
+        <button
+          className="absolute top-1/2 left-3 transform -translate-y-1/2 text-white text-5xl z-20 cursor-pointer select-none bg-transparent border-none"
+          onClick={prevImage}
+        >
+          &lt;
+        </button>
+        {images.map((image, index) => (
+          <div
+            className="absolute w-full h-full transition-transform duration-300"
+            style={{
+              transform: `
+                rotateY(${(currentImageIndex - index) * 50}deg)
+                scaleY(${1 + Math.abs(currentImageIndex - index) * -0.4})
+                translateZ(${Math.abs(currentImageIndex - index) * -30}rem)
+                translateX(${(currentImageIndex - index) * -5}rem)
+              `,
+              filter: `blur(${Math.abs(currentImageIndex - index)}rem)`,
+              opacity: Math.abs(currentImageIndex - index) === 0 ? 1 : 0, // Hide other cards
+              display: Math.abs(currentImageIndex - index) === 0 ? 'block' : 'none', // Show only the center card
+            }}
+            key={index}
+          >
+            <img
+              src={image}
+              alt={`day ${index + 1}`}
+              className="w-full h-full object-cover"
+            />
+            {getContentForIndex(index)}
+          </div>
+        ))}
+        <button
+          className="absolute top-36 right-3 transform translate-y-1/2 text-white text-5xl z-20 cursor-pointer select-none bg-transparent border-none"
+          onClick={nextImage}
+        >
+          &gt;
+        </button>
+      </div>
     </div>
-  </div>
-</div>
-
   );
-}
-
-function getContentForIndex(index) {
-  const contentMap = {
-    0: (
-      <div>
-        <p className="font-bold text-3xl">
-          Day 1 - #singlikesol (challenge, shorts, etc.)
-        </p>
-        <p className="pt-4 text-lg">
-          This day is characterized by covers of any of the Sauti Sol hits. At
-          least five from Myusiki with an interesting twist to them. At least
-          three group and two solos but as many as possible.
-        </p>
-      </div>
-    ),
-    1: (
-      <div>
-        <p className="font-bold text-4xl">Day 2 - #rewritesol</p>
-        <p className="pt-4 text-xl">
-          This day will be characterized by remixed versions of Sauti Sol songs.
-          At least five from Myusiki with an interesting twist to them. At least
-          three group and two solos but as many as possible.
-        </p>
-      </div>
-    ),
-    2: (
-      <div>
-        <p className="font-bold text-4xl">Day 3 - #shikanishasol</p>
-        <p className="pt-4 text-xl">
-          This day will involve the writing of mashups using the various Sauti
-          Sol songs. At least five from Myusiki with an interesting twist to
-          them. At least three group and two solos but as many as possible.
-        </p>
-      </div>
-    ),
-    3: (
-      <div>
-        <p className="font-bold text-4xl">Day 4 - #tuimbesol</p>
-        <p className="pt-4 text-xl">
-          This will be characterized by acapella song versions of Sauti Sol. At
-          least five from Myusiki with an interesting twist to them. At least
-          three group and two solos but as many as possible.
-        </p>
-      </div>
-    ),
-    4: (
-      <div>
-        <p className="font-bold text-4xl">Day 5 - #bonganasol</p>
-        <p className="pt-4 text-xl">
-          This day will be characterized by people giving their testimonies of
-          what Sauti Sol has done for them and the impact Sauti Sol has had in
-          their lives.
-        </p>
-      </div>
-    ),
-    5: (
-      <div>
-        <p className="font-bold text-4xl">Day 6 - #myfavouritesol</p>
-        <p className="pt-4 text-xl">
-          This will be characterized by people singing their favorite Sauti Sol
-          fam and saying why they like it and why it is their favorite.
-        </p>
-      </div>
-    ),
-    6: (
-      <div>
-        <p className="font-bold text-4xl">Day 7 - #asantesolfest</p>
-        <p className="pt-4 text-xl">
-          This day is to just thank Sauti Sol for the great work they have done
-          and sing their songs together. It is also where we will launch the
-          Asante Sol album.
-        </p>
-      </div>
-    ),
-  };
-
-  return contentMap[index] || <div></div>;
 }
