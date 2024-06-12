7437eea313afd0b4725e8a762affbb14d2f3cea3
arbitroy <ndauwa.austine20@students.dkut.ac.ke>
2024-06-12 17:59:23 +0300

    Added card

--- src/CSEvents.js
+++ src/CSEvents.js
@@ -37,6 +37,18 @@ function CSEvents() {
             </h3>
           </div>
         </div>
+        <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-5">
+          <div className="md:flex ">
+            <div className="md:flex-shrink-0">
+              <img className="h-48 w-full object-cover md:w-48" src="https://randomuser.me/api/portraits/men/1.jpg" alt="Event" />
+            </div>
+            <div className="p-8">
+              <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Event Name</div>
+              <p className="block mt-1 text-lg leading-tight font-medium text-black">Event Description</p>
+              <p className="mt-2 text-gray-500">Event Details...</p>
+            </div>
+          </div>
+        </div>
       </section>
       <section className='py-6 dark:bg-gray-500'>
         <div className="container flex flex-wrap items-center justify-center p-4 mx-auto sm:p-10">
